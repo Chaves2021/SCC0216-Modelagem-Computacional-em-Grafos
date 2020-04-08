5141a0949ca1734a7dcc32c8ab7f5ff44be1bda9
Viana2021 <caiomarcoschaves@gmail.com>
2020-04-07 21:43:15 -0300

    TAD graph finished

--- ex1/includes/graph.h
+++ ex1/includes/graph.h
@@ -1,12 +1,12 @@
 #ifndef _GRAPH_
 #define _GRAPH_
 
-typedef graph GRAPH;
+typedef struct graph GRAPH;
 
-GRAPH *graph_create(int, int)
-int graph_edge_insert(GRAPH *, int, int)
-int graph_edge_remove(GRAPH *, int, int)
-int graph_print_matrix(GRAPH)
-int graph_delete(GRAPH *)
+GRAPH *graph_create(int);
+int graph_edge_insert(GRAPH *, int, int);
+int graph_edge_remove(GRAPH *, int, int);
+int graph_print_matrix(GRAPH *);
+int graph_delete(GRAPH *);
 
 #endif
--- ex1/src/graph.c
+++ ex1/src/graph.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <graph.h>
+#include <defines.h>
 
 struct graph
 {
@@ -9,15 +10,15 @@ struct graph
 	int count_edges;
 };
 
-GRAPH *graph_create(int n_vertex, int n_edges)
+GRAPH *graph_create(int n_vertex)
 {
 	int i;
 	GRAPH *graph;
-	graph = (graph *) calloc(1, sizeof(graph));
+	graph = (GRAPH *) calloc(1, sizeof(GRAPH));
 
 	graph->matrix = (int **) malloc(n_vertex * sizeof(int *));
 	for(i = 0; i < n_vertex; i++) graph->matrix[i] = (int *) calloc(n_vertex, sizeof(int));
-	count_vertex = n_vertex;
+	graph->count_vertex = n_vertex;
 
 	return graph;
 }
@@ -27,8 +28,8 @@ int graph_edge_insert(GRAPH *graph, int vertex0, int vertex1)
 	if(!graph) return ERROR;
 	if(!graph->matrix) return ERROR;
 
-	graph->matrix[vertex0 - 1][vertex1 - 1] = graph->matrix[vertex1 - 1][vertex0 - 1] = 1;
-	count_edges++;
+	graph->matrix[vertex0][vertex1] = graph->matrix[vertex1][vertex0] = 1;
+	graph->count_edges++;
 
 	return SUCCESS;
 }
@@ -38,8 +39,8 @@ int graph_edge_remove(GRAPH *graph, int vertex0, int vertex1)
 	if(!graph) return ERROR;
 	if(!graph->matrix) return ERROR;
 
-	graph->matrix[vertex0 - 1][vertex1 - 1] = graph->matrix[vertex1 - 1][vertex0 - 1] = 0;
-	count_edges--;
+	graph->matrix[vertex0][vertex1] = graph->matrix[vertex1][vertex0] = 0;
+	graph->count_edges--;
 
 	return SUCCESS;
 }
@@ -48,12 +49,14 @@ int graph_print_matrix(GRAPH *graph)
 {
 	if(!graph) return ERROR;
 	if(!graph->matrix) return ERROR;
+	int i, j;
 
 	for(i = 0; i < graph->count_vertex; i++)
 	{
 		for(j = 0; j < graph->count_vertex; j++) printf("%d ", graph->matrix[i][j]);
 		printf("\n");
 	}
+	printf("\n");
 	return SUCCESS;
 }
 
@@ -61,8 +64,11 @@ int graph_delete(GRAPH *graph)
 {
 	if(!graph) return ERROR;
 	if(!graph->matrix) return ERROR;
+	int i;
 
-	for(i = 0; i < graph->n_vertex; i++) free(graph->matrix[i]);
+	for(i = 0; i < graph->count_vertex; i++) free(graph->matrix[i]);
 	free(graph->matrix);
 	free(graph);
+
+	return SUCCESS;
 }
